f64fdc99f501466a324df03fa3095a857f6b9dd5
remijang <b11902051@csie.ntu.edu.tw>
2023-05-28 02:45:03 +0800

    owo

---
+++ count.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+    FILE *fin = fopen(argv[1], "rb");
+    int count[10] = {}, t;
+    while(fscanf(fin, "%d", &t) != EOF){
+        count[t]++;
+    }
+    for(int i = 0; i < 10; ++i) printf("%d  ", count[i]);
+    putchar('\n');
+    return 0;
+}
