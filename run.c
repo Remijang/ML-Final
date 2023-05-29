8b78363ee6f1d43118bfad2e0ed71032a4604005
remijang <b11902051@csie.ntu.edu.tw>
2023-05-29 22:23:34 +0800

    owo

--- run.c
+++ run.c
@@ -5,8 +5,8 @@ int main(){
     float cs = 0.5316;
     float scale = 1000;
     char run[100];
-    for(float i = 10; i < 300; i *= 2){
-        for(float j = 10; j < 31; j += 10){
+    for(float i = 10; i <= 200; i += 10){
+        for(float j = 5; j <= 25; j += 5){
                 sprintf(run, "./run.sh 5 %f %f", i, j);
                 printf("%f %f\n", i, j);
                 system(run);
